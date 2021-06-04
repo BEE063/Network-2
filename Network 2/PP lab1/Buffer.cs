f415173ca09274fe526a751d7c81eeff80c05283
BEE063 <antonova.hellokitty2011@yandex.ru>
2021-06-04 22:08:31 +0300

    add network 2

---
+++ Network 2/PP lab1/Buffer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PP_lab1
+{
+    public class Buffer
+    {
+        public BitArray frame { get; set; }
+        public BitArray receipt { get; set; }
+        public BitArray request { get; set; }
+        public BitArray[] _frameArray;
+        public Buffer()
+        {
+
+        }
+    }
+}
