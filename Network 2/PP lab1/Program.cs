7e38854b4fd145802e2c1354c296ec77ecd9bc5e
BEE063 <antonova.hellokitty2011@yandex.ru>
2021-06-04 22:02:10 +0300

    add network 2

---
+++ Network 2/PP lab1/Program.cs
@@ -0,0 +1,57 @@
+﻿using PP_lab1;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadsEdu
+{
+    public delegate void PostReceiptToFirstWt(BitArray message);
+    public delegate void PostDataToSecondWT(BitArray message);
+    public delegate void PostDataToFirstWt(BitArray message);
+    public delegate void PostReceiptToSecondWt(BitArray message);
+    public delegate void PostRequestToSecondWt(BitArray message);
+    public delegate void PostRequestToFirstWt(BitArray message);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleHelper.WriteToConsole("Главный поток", "");
+            Semaphore firstReceiveSemaphore = new Semaphore(0, 1);
+            Semaphore secondReceiveSemaphore = new Semaphore(0, 1);
+
+            FirstStation firstStation = new FirstStation(ref secondReceiveSemaphore, ref firstReceiveSemaphore);
+            SecondStation secondStation = new SecondStation(ref firstReceiveSemaphore, ref secondReceiveSemaphore);
+
+            Thread threadFifth = new Thread(new ParameterizedThreadStart(firstStation.SendRequestToSecond));
+            Thread threadSixth = new Thread(new ParameterizedThreadStart(secondStation.SendRequestToFirst));
+            Thread threadFirst = new Thread(new ParameterizedThreadStart(firstStation.SendDataToSecond));
+            Thread threadSecond = new Thread(new ParameterizedThreadStart(secondStation.SendReceiptToFirst));
+            Thread threadThird = new Thread(new ParameterizedThreadStart(secondStation.SendDataToFirst));
+            Thread threadFourth = new Thread(new ParameterizedThreadStart(firstStation.SendReceiptToSecond));
+            
+
+
+            PostReceiptToFirstWt postReciptToFirstWt = new PostReceiptToFirstWt(firstStation.ReceiveReceiptFromSecond);
+            PostDataToSecondWT postDataToSecondWt = new PostDataToSecondWT(secondStation.ReceiveDataFromFirst);
+            PostDataToFirstWt postDataToFirstWt = new PostDataToFirstWt(firstStation.ReceiveDataFromSecond);
+            PostReceiptToSecondWt postReceiptToSecondWt = new PostReceiptToSecondWt(secondStation.ReceiveReceiptFromFirst);
+            PostRequestToFirstWt postRequestToFirstWt = new PostRequestToFirstWt(firstStation.ReceiveRequestFromSecond);
+            PostRequestToSecondWt postRequestToSecondWt = new PostRequestToSecondWt(secondStation.ReceivedRequestFromFirst);
+
+            threadFifth.Start(postRequestToSecondWt);
+            threadSixth.Start(postRequestToFirstWt);
+            threadFirst.Start(postDataToSecondWt);
+            threadSecond.Start(postReciptToFirstWt);
+            threadThird.Start(postDataToFirstWt);
+            threadFourth.Start(postReceiptToSecondWt);
+
+            Console.ReadLine();
+
+        }
+    }
+    
+
+}
---
+++ Network 2/PP lab1/SecondStation.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using ThreadsEdu;
+
+namespace PP_lab1
+{
+    public class SecondStation
+    {
+        private Semaphore _sendSemaphore;
+        private Semaphore _receiveSemaphore;
+        private BitArray _receivedMessageFromFirst;
+        private BitArray _receivedReceiptFromFirst;
+        private BitArray _receivedRequestFromFirst;
+        private BitArray _sendReceipt;
+        private BitArray _sendRequest;
+        private PostReceiptToFirstWt _postReceiptToFirst;
+        private PostDataToFirstWt _postDataToFirst;
+        private PostRequestToFirstWt _postRequestToFirst;
+        private BitArray _sendData;
+        private bool _dataIsReceived = false;
+
+
+        public SecondStation(ref Semaphore sendSemaphore, ref Semaphore receiveSemaphore)
+        {
+            _sendSemaphore = sendSemaphore;
+            _receiveSemaphore = receiveSemaphore;
+        }
+        public void SendReceiptToFirst(Object obj)
+        {
+            _postReceiptToFirst = (PostReceiptToFirstWt)obj;
+            _receiveSemaphore.WaitOne();
+
+            _sendReceipt = new BitArray(1);
+
+            Buffer buffer = new Buffer();
+            buffer.frame = _receivedMessageFromFirst;
+            buffer.request = _receivedReceiptFromFirst;
+
+            if (buffer.frame != null && _dataIsReceived==true)
+            {
+                ConsoleHelper.WriteToConsoleReceipt("2 поток", _receivedRequestFromFirst);
+                ConsoleHelper.WriteToConsoleArray("2 поток получил данные", buffer.frame);
+                ConsoleHelper.WriteTextMessageToConsole(buffer.frame);
+                Frame.GenerateReceipt(_sendReceipt, true);
+                _dataIsReceived = false;
+            }
+            else
+            {
+                Frame.GenerateReceipt(_sendReceipt, false);
+            }
+            ConsoleHelper.WriteToConsole("2 поток", "Готовлю данные для передачи");
+            ConsoleHelper.WriteToConsole("2 поток", "Данные переданы");
+
+            _postReceiptToFirst(_sendReceipt);
+
+            
+            _sendSemaphore.Release();
+            _receiveSemaphore.WaitOne();
+            if (_dataIsReceived == true)
+            {
+                ConsoleHelper.WriteToConsoleArray("2 поток получил данные", buffer.frame);
+                ConsoleHelper.WriteTextMessageToConsole(buffer.frame);
+                Frame.GenerateReceipt(_sendReceipt, true);
+                _sendSemaphore.Release();
+            }
+            ConsoleHelper.WriteToConsoleRequest("2 поток", "", buffer.request);
+            
+        }
+        public void SendDataToFirst(Object obj)
+        {
+            _postDataToFirst = (PostDataToFirstWt)obj;
+            _sendData = new BitArray(64);
+            _postDataToFirst(Frame.GenerateData("World"));
+            
+        }
+        public void SendRequestToFirst(object obj)
+        {
+            _postRequestToFirst = (PostRequestToFirstWt)obj;
+            _sendRequest = new BitArray(1);
+            Frame.GenerateReceipt(_sendRequest, true);
+            _postRequestToFirst(_sendRequest);
+            _sendSemaphore.Release();
+        }
+        public void ReceivedRequestFromFirst(BitArray array)
+        {
+            _receivedRequestFromFirst = array;
+        }
+        public void ReceiveDataFromFirst(BitArray array)
+        {
+            _receivedMessageFromFirst = array;
+            _dataIsReceived = true;
+        }
+        public void ReceiveReceiptFromFirst(BitArray array)
+        {
+            _receivedReceiptFromFirst = array;
+        }
+    }
+}
